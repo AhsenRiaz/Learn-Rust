ecea928af34a8e88986ae38ce22d4a6ee7f74ffc
ahsenriaz <ahsenriaz2001@gmail.com>
2023-01-04 16:12:38 +0500

    strings

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,3 +1,41 @@
 fn main() {
     println!("Hello, world!");
+    create_strings();
+    update_string();
+    format_macro();
+}
+
+fn create_strings() {
+    let s = String::new();
+
+    let data = "initial contents";
+    let s = data.to_string();
+    println!("string: {}", s);
+
+    let s = String::from("hello");
+
+    let hello = String::from("السلام عليكم");
+    println!("{}", hello);
+}
+
+fn update_string() {
+    let mut s = String::from("foo");
+
+    s.push_str("bar");
+    println!("{}", s);
+}
+
+fn concatenation() {
+    let s1 = String::from("hello");
+    let s2 = String::from("world");
+    let s3 = s1 + &s2;
+}
+
+fn format_macro() {
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{s1}-{s2}-{s3}");
+    println!("{}", s);
 }
